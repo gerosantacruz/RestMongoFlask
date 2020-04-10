1638984c5412c54e4a626365e5eca3b0582de1a5
gerosantacruz <geronimo.santacruz2017@gmail.com>
2020-04-10 18:11:22 -0300

    we have connection and response

--- src/app.py
+++ src/app.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, request
+from flask_pymongo import PyMongo
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.config['MONGO_URI']:'mongodb://localhost/pytmongodb'
+
+@app.route('/users', methods=['POST'])
+
+def create_user():
+    #recieve data
+    username = request.json['username']
+    password = request.json['password']
+    email = request.json['email']
+    return {'message':'receive'}
+
 
 
 if __name__ == "__main__":
-        app.run(debug=true)
+        app.run(debug=True)
