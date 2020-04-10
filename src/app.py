1653d3dac0f540e8a5dd2f74aff21e5e15c926ab
gerosantacruz <geronimo.santacruz2017@gmail.com>
2020-04-10 19:08:59 -0300

    The app connect to the databes an see positive response using postman

--- src/app.py
+++ src/app.py
@@ -3,16 +3,30 @@ from flask_pymongo import PyMongo
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.config['MONGO_URI']:'mongodb://localhost/pytmongodb'
+app.config["MONGO_URI"] = "mongodb://localhost:27017/pymongodb"
+mongo = PyMongo(app)
 
 @app.route('/users', methods=['POST'])
-
 def create_user():
     #recieve data
     username = request.json['username']
     password = request.json['password']
     email = request.json['email']
-    return {'message':'receive'}
+
+    if username and password and email:
+        hashedpassword = generate_password_hash(password)
+        id= mongo.db.users.insert(
+            {'username':username, 'email':email, 'password':password}
+        )
+        response = {
+            'id': str(id),
+            'username': username,
+            'password': hashedpassword,
+            'email': email,
+        }
+        return response
+    else:
+        return {'message':'receive'}
 
 
 
