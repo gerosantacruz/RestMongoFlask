3436666ba5017c86e900d0340481cce2aed76cf6
gerosantacruz <geronimo.santacruz2017@gmail.com>
2020-04-11 18:19:59 -0300

    create, get all user and get specific user works

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify, Response
 from flask_pymongo import PyMongo
 from werkzeug.security import generate_password_hash, check_password_hash
+from bson import json_util
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/pymongodb"
@@ -26,8 +28,29 @@ def create_user():
         }
         return response
     else:
-        return {'message':'receive'}
+        return not_found()
 
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = mongo.db.users.find()
+    response = json_util.dumps(users)
+    return Response(response, mimetype='application/json')
+
+@app.route('/users/<id>', methods=['GET'])
+def get_user_id(id):
+   user = mongo.db.users.find_one({'_id': ObjectId(id)})
+   response = json_util.dumps(user)
+   return Response(response, mimetype='application/json')
+    
+
+@app.errorhandler(404)
+def not_found(error=None):
+    response = jsonify({
+        'message':'Resoource not found: ' + request.url,
+        'Status': 404
+    })   
+    response.status_code = 404
+    return response 
 
 
 if __name__ == "__main__":
